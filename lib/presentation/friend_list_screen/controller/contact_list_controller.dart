033ddf24608ae046d90931d2dd79bc2214915e01
blazingcoder001 <jrpjishnuraj@gmail.com>
2024-01-22 11:52:50 -0500

    Changes made:- -> Moved contactListItemModel variable declaration to inside conditional block in contact_list_controller.dart. This was done to remove the bug in duplication of search results in contact_list_screen.dart.

--- lib/presentation/friend_list_screen/controller/contact_list_controller.dart
+++ lib/presentation/friend_list_screen/controller/contact_list_controller.dart
@@ -1,3 +1,4 @@
+
 import 'package:blaze_call/core/app_export.dart';
 import 'package:blaze_call/core/utils/authorization.dart';
 import 'package:blaze_call/presentation/friend_list_screen/models/contact_list_model.dart';
@@ -22,24 +23,31 @@ class FriendListController extends GetxController {
   Stream <Rx<List<ContactListItemModel>>?> getContacts(String searchString) {
 
     Rx<List<ContactListItemModel>> allContacts = Rx<List<ContactListItemModel>>([]);
-    ContactListItemModel x=ContactListItemModel();
 
     final requestsRef = FirebaseFirestore.instance.collection('users');
 
     return requestsRef.snapshots().map((querySnapshot) {
       for (var doc in querySnapshot.docs) {
+
         Authorization authorization=Authorization();
         authorization.initializeAuthorization();
-        if(searchString != '' && doc.id.contains(searchString) && doc.data()['UID']!= authorization.uid) {// Added condition to check if the search string is equal
-          //to the account signed in.
+        if(searchString != '' && doc.id.contains(searchString)  && doc.data()['UID']!=authorization.uid)
+        {
+          ContactListItemModel x=ContactListItemModel();// the reason why we are creating a new instance of x inside each iteration is because,
+          // x shares the same memory location. Even though we update the value of x in the second iteration, the value of x added in the previous iteration also changes. That is
+          //why getting same multiple list values.
           print("searchString: "+searchString);
           x.userName?.value = doc.id;
+          // print("x.username: "+x.userName!.value+"doc.id: "+doc.id);
           x.firstName?.value = doc.data()['First Name'];
           x.lastName?.value = doc.data()['Last Name'];
           x.profilePicUrl?.value=doc.data()['profilePicURL'];
-          allContacts!.value.add(x);
+          print(x.userName);
+          allContacts.value.add(x);
         }
       }
+      // print(allContacts.value[0].userName!);
+      // print(allContacts.value[1].userName!);
       return allContacts;
     });
   }
